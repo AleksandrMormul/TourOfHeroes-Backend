b85fda95d1a57d2ffb8644c745751aa4c3aecf10
dev <alex.mormul.jsn@gmail.com>
2019-09-26 12:17:27 +0300

    env configuration

--- model/Hero.js
+++ model/Hero.js
@@ -1,19 +1,6 @@
 const mongo = require('mongoose');
 const Schema = mongo.Schema;
 
-//configDb.hz;
-
-/*mongo.Promise = global.Promise;
-
-mongo.connect(configDb.connection,{useUnifiedTopology: true, useNewUrlParser: true}).then(
-  () => {
-    console.log('Database is connected!')
-  },
-  err => {
-    console.log('Can not connect to the database' + err)
-  }
-);*/
-
 let Hero = new Schema({
   name: {
     type: String
--- settings/connection.js
+++ settings/connection.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-  connection: 'mongodb://127.0.0.1/TourOfHeroes'
+  connection: process.env.DB_HOST
 };
