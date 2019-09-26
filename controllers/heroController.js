8d55f8249ec07f36182630f344afd8c79bb8be1d
dev <alex.mormul.jsn@gmail.com>
2019-09-26 13:34:35 +0300

    added async/await func

--- controllers/heroController.js
+++ controllers/heroController.js
@@ -3,40 +3,50 @@ const db = require('../model/connectionDB');
 db.db;
 
 exports.hero_detail = function(req, res) {
-  Hero.find(function (err, heroes){
-    if(err){
+  Hero.find(async (err, heroes) => {
+    try {
+     await res.json(heroes);
+    } catch (err) {
       console.log(err);
-    }
-    else {
-      res.json(heroes);
     }
   });
 };
 
 exports.hero_get = function(req, res) {
   const id = req.params.id;
-  Hero.findOne({_id: id}, function(err, heroes){
-    if(err) return console.log(err);
-    res.send(heroes);
-  });
+    Hero.findOne({_id: id}, async (err, heroes) => {
+      try {
+        await res.send(heroes);
+      }
+      catch (err) {
+        console.log(err);
+      }
+    });
 };
 
 exports.hero_post = function(req, res) {
   if(!req.body) return res.sendStatus(400);
   const nameHero = req.body.name;
   const hero = new Hero({name: nameHero});
-  hero.save(function(err){
-    if(err) return console.log(err);
-    res.send(hero);
+  hero.save(async err => {
+    try {
+      await res.send(hero);
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
 
 exports.hero_delete = function(req, res) {
   const id = req.params.id;
-  Hero.findByIdAndDelete(id, function(err, heroes){
-   if(err) return console.log(err);
-      res.send(heroes);
-    });
+  Hero.findByIdAndDelete(id, async(err, heroes) => {
+    try {
+      await res.send(heroes);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  });
 };
 
 exports.hero_update = function(req, res) {
@@ -44,9 +54,12 @@ exports.hero_update = function(req, res) {
   const id = req.body._id;
   const heroName = req.body.name;
   const newHero = {name: heroName};
-  Hero.findOneAndUpdate({_id: id}, newHero, {new: true}, function(err, hero){
-    if(err) return console.log(err);
-    res.send(hero);
+  Hero.findOneAndUpdate({_id: id}, newHero, {new: true}, async (err, hero) => {
+    try {
+      await res.send(hero);
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
 
